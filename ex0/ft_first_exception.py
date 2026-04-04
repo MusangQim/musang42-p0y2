628e219f391efe31b2d3a0e47d566430b5da7f8c
MusangQim <musangqim03@gmail.com>
2026-04-05 02:32:42 +0800

    update and looking for errors

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,13 +1,22 @@
-def input_temperature(temp_str) -> int:
+def input_temperature(temp_str: str) -> int:
     try:
         return int(temp_str)
-    except Exception as e:
-        error ="Caught input_temperature error: invalid literal for int() with base 1): 'abc'"
-        print(error)
+    except Exception as error:
+        print(f"Caught input_temperature error: {error}")
 
-def test_temperature():
+
+def test_temperature() -> None:
     number_input = input_temperature("25")
     print(f"Input data is '{number_input}'")
     print(f"Temperature is now {number_input}°C")
+    print()
+    print("Input data is 'abc'") 
     str_input = input_temperature("abc")
-    print(f"Input data is '{str_input}'")
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===")
+    print()
+    test_temperature()
+    print()
+    print("All tests completed - program didn't crash!")
