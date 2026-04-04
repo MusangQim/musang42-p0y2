84c5f651893fa0907a7823b9806d7a78d9e1e134
MusangQim <musangqim03@gmail.com>
2026-04-05 02:18:56 +0800

    update

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,12 +1,13 @@
 def input_temperature(temp_str) -> int:
     try:
-        return int(input(temp_str))
+        return int(temp_str)
     except:
-        print(f"Caught input_temperature error: invalid literal for int() with base 1): 'abc'")
+        error ="Caught input_temperature error: invalid literal for int() with base 1): 'abc'"
+        print(error)
 
 def test_temperature():
-    number_input = input_temperature(25)
+    number_input = input_temperature('25')
     print(f"Input data is '{number_input}'")
     print(f"Temperature is now {number_input}°C")
-    str_input = input_temperature(abc)
+    str_input = input_temperature('abc')
     print(f"Input data is '{str_input}'")
