5a6bf64d25bfbee57e7b380009c7b1cf7c836fd4
MusangQim <musangqim03@gmail.com>
2026-04-07 12:51:25 +0800

    add classes and functions

--- ex3/ft_custom_errors.py
+++ ex3/ft_custom_errors.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
-class GardenError(Exception)
+class GardenError(Exception):
+    def 
+
+
 class PlantError(GardenError)
+
+
 class WaterError(GardenError)
+
+def test_garden_error()
+
+
+def test_plant_error()
+
+
+def test_water_error()
+
+
 if __name__ == "__main__":
     print("=== Custom Garden ===")
+    print()
+    print("All custom error types work correctly!")
